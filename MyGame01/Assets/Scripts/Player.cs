7c04b9b49f1652ea11e62860f69241d7204bde27
HelloZOOO <qnrwnd10119@gmail.com>
2023-03-17 02:00:03 +0900

    플레이어 애니메이션 구현 단순 포스트프로세싱

--- MyGame01/Assets/Scripts/Player.cs
+++ MyGame01/Assets/Scripts/Player.cs
@@ -3,40 +3,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem; //새롭게 설치한 Player Input System 을 사용하기 위해 임포트
-
 public class Player : MonoBehaviour
 {
     public Vector3 inputVec;
     public float speed;
     Rigidbody rigid;
-    
+    Animator anim;
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
-    }
-
-    void Update()
-    {
-        //input키에 Horizontal 은 [left, right] Vertical 은 [up, down] 키가 매핑되어있음
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.z = Input.GetAxisRaw("Vertical");
+        anim = GetComponentInChildren<Animator>();
     }
 
     //FixedUdate는 물리 연산 프레임마다 호출되는 함수다
     void FixedUpdate() 
     {
+        Move();
+    }
+
+    void LateUpdate()
+    {
+        Turn();
+    }
+
+    //플레이어 이동 구현
+    void Move()
+    {
+        //input키에 Horizontal 은 [left, right] Vertical 은 [up, down] 키가 매핑되어있음
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.z = Input.GetAxisRaw("Vertical");
+
         //어느 방향이든 벡터값을 1로 고정
         Vector3 nextVec = inputVec.normalized * speed * Time.deltaTime;
 
-        //3.위치 이동
+        // ... 위치 이동
         //MovePostion은 위치 이동이라 현재 위치를 더해줘야함
         //이 코드에서 현재 위치는 rigid.postion 이다
         //인풋값과 현재위치를 더해주면 플레이어가 나아가야 할 방향을 계산한다
         rigid.MovePosition(rigid.position + nextVec);
+        if(nextVec != Vector3.zero)
+            anim.SetFloat("RunState", 0.5f);
+        else
+            anim.SetFloat("RunState", 0.0f);
     }
 
-    void LateUpdate()
+    //플레이어 회전 구현
+    void Turn()
     {
         if (inputVec.x != 0)
             this.transform.localScale = new Vector3(-1.5f, 1.5f, 1.5f);
