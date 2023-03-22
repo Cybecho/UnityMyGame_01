d14c628c308323b302932eba38f6fd34bf9bf440
HelloZOOO <qnrwnd10119@gmail.com>
2023-03-22 14:49:04 +0900

    RandomPatternTile 구현

--- MyGame01/Assets/Scripts/Reposition.cs
+++ MyGame01/Assets/Scripts/Reposition.cs
@@ -6,15 +6,35 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    public Transform groundSize; //맵의 scale x 값을 불러오기 위함
+    // ... 프리팹 보관할 변수
+    public GameObject[] prefabs;
 
-    public Transform groundSize;
+    // ... 풀 담당을 하는 리스트들
+    //리스트도 프리팹의 개수만큼 생성되어야하니 배열로 선언해준다
+    List<GameObject>[] prefabsList;
+    
+    void Awake() 
+    {
+        //리스트기때문에 new를 만들어줘야함
+        //List 배열의 크기는 prefabs배열과 동일하기때문에 배열에 Prefabs의 길이를 넣어준다
+        prefabsList = new List<GameObject>[prefabs.Length];
+
+        //for문으로 배열 내부 오브젝트들을 모두 초기화해준다
+        for (int index = 0; index < prefabsList.Length; index++)
+        {
+            //풀을 담는 배열도 초기화해주고 각각의 리스트들도 전부 초기화해줌
+            prefabsList[index] = new List<GameObject>();
+        }
+
+        randTile();
+    }
 
     void Update() 
     {
         moveMap();
     }
-    //트리거가 체크된 콜라이더를 벗어났을때
-    //이를 위해 플레이어에 Area를 만들었다 (충돌여부를 확인하기 위해)
+
     void moveMap()
     {
         //프리팹의 크기를 자동으로 계산
@@ -22,15 +42,13 @@ public class Reposition : MonoBehaviour
 
         // ... 거리를 구하기 위해 플레이어 위치와 타일맵 위치를 미리 저장
         
-        //플레이어 위치
-        //플레이어 포지션을 가져오기위해 게임매니저에 저장해둔 플레이어에서 위치값을 가져오는 과정
+        //플레이어의 위치를 가져오기위해 게임매니저에 저장해둔 플레이어에서 위치값을 가져오는 과정
         Vector3 playerPos = GameManager.instance.player.transform.position;
         
-        //타일맵 위치
         //현재 이 스크립트가 들어있는 타일맵의 위치값
         Vector3 tilePos = transform.position; 
 
-        //x축과 z축 각각의 거리를 구하는 코드
+        //플레이어와 타일 사이의 x축의 거리를 구함
         //Mathf 는 수학 라이브러리.. 절대값을 구하기 위해 Abs를 불러옴
         float diffX = Mathf.Abs(playerPos.x - tilePos.x);
 
@@ -41,7 +59,20 @@ public class Reposition : MonoBehaviour
         float dirX = playerDir.x < 0 ? -1 : 1;
 
         //일정거리 이동했을때 플레이어에서 가장 먼 프리팹을 플레이어 방향으로 이동
+        //맵이 이동할때마다 맵 디자인이 랜덤으로 변합니다
         if (diffX >= (mapSize.x * 3))
+        {
             transform.Translate(Vector3.right * dirX * (mapSize.x * 5));
+            randTile();
+        }
+    }
+
+    //prefab에 등록한 프리팹을 초기화하고 랜덤으로 Active 하는 코드
+    void randTile()
+    {
+        for (int index = 0; index < prefabsList.Length; index++)
+            prefabs[index].SetActive(false);
+        
+        prefabs[Random.Range(0, prefabsList.Length)].SetActive(true);
     }
 }
